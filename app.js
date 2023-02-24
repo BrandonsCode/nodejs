1ea44a22b1378e6ef80b24143ae9f630b341ff0c
BrandonsCode <brandonscpucode@gmail.com>
2023-02-24 01:23:02 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end('Hello World');
+});
+
+server.listen(3000, '127.0.0.1',() => {
+    console.log('Server running...');
+});
---
+++ app2.js
@@ -0,0 +1,3 @@
+﻿const { v4:uuidv4 } = require('uuid');
+
+console.log(uuidv4());
